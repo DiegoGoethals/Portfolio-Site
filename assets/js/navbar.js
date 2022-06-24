7f0d399eed77da9bd603b17912f2ef6b3ce8728a
diego <goethalsdiego@gmail.com>
2022-06-24 12:13:44 +0200

    refactored structure and added navbar.js to avoid html duplication

---
+++ assets/js/navbar.js
@@ -0,0 +1,11 @@
+"use strict";
+
+document.addEventListener("DOMContentLoaded", init);
+
+function init() {
+    document.querySelector("nav").insertAdjacentHTML("beforeend",
+        `<a class="navbutton" type="button" href="index.html">Home</a>
+        <a class="navbutton" type="button" href="about.html">About me</a>
+        <a class="navbutton" type="button" href="projects.html">Projects</a>
+        <a href="index.html#contact" class="navbutton">Contact</a>`);
+}
