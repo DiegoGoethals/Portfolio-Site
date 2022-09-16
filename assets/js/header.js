a4fc70cbb9201b8b663501ef7b4fad2d7837e44a
diego <goethalsdiego@gmail.com>
2022-09-16 17:55:11 +0200

    Made index and about one html page divided in sections

--- assets/js/header.js
+++ assets/js/header.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", init);
 function init() {
     document.querySelector("nav").insertAdjacentHTML("beforeend",
         `<a class="navbutton" type="button" href="index.html">Home</a>
-        <a class="navbutton" type="button" href="about.html">About me</a>
+        <a class="navbutton" type="button" href="#about">About me</a>
         <a class="navbutton" type="button" href="projects.html">Projects</a>
         <a href="index.html#contact" class="navbutton">Contact</a>`);
     document.querySelector("header").insertAdjacentHTML("afterbegin", `<h1>Diego Goethals</h1>`);
