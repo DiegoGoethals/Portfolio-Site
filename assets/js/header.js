b26ddf3761708b08b31789bf5ea429f2f6c090b8
diego <goethalsdiego@gmail.com>
2022-09-19 15:02:41 +0200

    Created nice scroll effect when clicking links, made homepage more readable and aligned text in the about section better

--- assets/js/header.js
+++ assets/js/header.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", init);
 
 function init() {
     document.querySelector("nav").insertAdjacentHTML("beforeend",
-        `<a class="navbutton" type="button" href="index.html">Home</a>
+        `<a class="navbutton" type="button" href="#home">Home</a>
         <a class="navbutton" type="button" href="#about">About me</a>
         <a class="navbutton" type="button" href="projects.html">Projects</a>
         <a href="index.html#contact" class="navbutton">Contact</a>`);
