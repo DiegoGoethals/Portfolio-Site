9c4ae2fa42c41cd447020e2362005cbd5b938c84
diego <goethalsdiego@gmail.com>
2022-09-19 17:47:45 +0200

    Made projects one with index now

--- assets/js/header.js
+++ assets/js/header.js
@@ -6,7 +6,7 @@ function init() {
     document.querySelector("nav").insertAdjacentHTML("beforeend",
         `<a class="navbutton" type="button" href="#home">Home</a>
         <a class="navbutton" type="button" href="#about">About me</a>
-        <a class="navbutton" type="button" href="projects.html">Projects</a>
-        <a href="index.html#contact" class="navbutton">Contact</a>`);
+        <a class="navbutton" type="button" href="#projects">Projects</a>
+        <a href="#contact" class="navbutton">Contact</a>`);
     document.querySelector("header").insertAdjacentHTML("afterbegin", `<h2>Diego Goethals</h2>`);
 }
