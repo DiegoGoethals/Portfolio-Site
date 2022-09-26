7fae3307a93b4d813788d96c4b6fbe0f32acbcdc
diego <goethalsdiego@gmail.com>
2022-09-26 16:35:59 +0200

    added certificates and a way to display them to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,10 @@ document.addEventListener("DOMContentLoaded", init);
 
 function init() {
     setAge();
+    document.querySelectorAll(".certificate").forEach(certificate => {
+        certificate.addEventListener("click", openPDF);
+    });
+    document.getElementById("close").addEventListener("click", off);
 }
 
 function setAge() {
@@ -12,4 +16,19 @@ function setAge() {
 
 function calculateAge(birthday) {
     return new Date(new Date() - birthday).getFullYear() - 1970;
+}
+
+function openPDF(e) {
+    const skill = e.target.innerText;
+    const overlay = document.getElementById("overlay");
+    overlay.insertAdjacentHTML("beforeend", `
+    <img class="imgOverlay"
+    src="assets/certificates/${skill}.jpg" alt="${skill} certificate"</img>`);
+    overlay.style.display = "block";
+}
+
+function off() {
+    const overlay = document.getElementById("overlay")
+    overlay.style.display = "none";
+    overlay.removeChild(document.querySelector(".imgOverlay"));
 }
